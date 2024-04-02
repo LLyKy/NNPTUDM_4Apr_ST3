2c77998a0df1d329a1c6c1542ac382dcd2d17d69
LLyKy <lieulyky1@gmail.com>
2024-04-02 20:30:29 +0700

    feat(auth: password): firstcommit add new reset pass

---
+++ configs/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    SECRET_KEY: 'TUNG123',
+    EXPIRE_JWT: '1d',
+    hostName: 'localhost:3000',
+    Host: "sandbox.smtp.mailtrap.io",
+    Port: 2525,
+    Username: "a1ca2784e67355",
+    Password: "ac7f97da9e4aa7"
+}
+
+// var transport = nodemailer.createTransport({
+//     host: "sandbox.smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "a1ca2784e67355",
+//       pass: "ac7f97da9e4aa7"
+//     }
+//   });
